d45ae6532d0fa3727ce6bf06270368d017cebdee
calvinsim2 <88623537+calvinsim2@users.noreply.github.com>
2024-04-27 21:51:50 +0800

    added formControl for codeGenre

--- src/app/game/add-game/add-game.component.ts
+++ src/app/game/add-game/add-game.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { GameService } from '../service/game.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { DeveloperService } from '../service/developer.service';
 import { take } from 'rxjs';
 import { DeveloperModel } from '../model/developer.model';
 import { CodesTableService } from '../service/codes-table.service';
-import { CodesTableModel } from '../model/codesTable.model';
+import { CodesTableModel, GenreFormControls } from '../model/codesTable.model';
 
 @Component({
   selector: 'app-add-game',
@@ -27,87 +32,111 @@ export class AddGameComponent {
   public codeMatureRatingList: CodesTableModel[] = [];
   public codeGenreList: CodesTableModel[] = [];
   public codePlatformList: CodesTableModel[] = [];
-  public isGenreListPopulated: boolean = false;
 
   ngOnInit() {
-    this.getAllRequiredData()
-    // addControl 
-    // new mapped array, 
     this.gameSubmissionForm = this.formBuilder.group({
       name: ['', Validators.required],
       description: ['', Validators.required],
       codeMatureRating: ['', Validators.required],
-      concaetenatedCodeGenre: ['', Validators.required],
+      concatenatedCodeGenre: ['', Validators.required],
       price: [0, Validators.min(0)],
-      developerId: [this.developerList.length > 0 ? this.developerList[0].name : '', Validators.required],
+      developerId: [
+        this.developerList.length > 0 ? this.developerList[0].name : '',
+        Validators.required,
+      ],
     });
 
+    this.getAllRequiredData();
   }
 
   getAllRequiredData() {
-    this.getAllDevelopers()
-    this.getAllCodeMatureRating()
-    this.getAllCodeGenre()
-    this.getAllCodePlatform()
+    this.getAllDevelopers();
+    this.getAllCodeMatureRating();
+    this.getAllCodeGenre();
+    this.getAllCodePlatform();
   }
 
   getAllDevelopers() {
-    this.developerService.getAllDeveloper().pipe(take(1)).subscribe({
-      next: (res) => {
-        this.developerList = res;
-      },
-      error: (err) => {
-        alert(`An error has occured. Please try again later `);
-      },
-    }
-      
-    )
+    this.developerService
+      .getAllDeveloper()
+      .pipe(take(1))
+      .subscribe({
+        next: (res) => {
+          this.developerList = res;
+        },
+        error: (err) => {
+          alert(`An error has occured. Please try again later `);
+        },
+      });
   }
 
   getAllCodeMatureRating() {
-    this.codesTableService.getAllCodeMatureRating().pipe(take(1)).subscribe({
-      next: (res) => {
-        this.codeMatureRatingList = res;
-      },
-      error: (err) => {
-        alert(`An error has occured. Please try again later `);
-      },
-    })
+    this.codesTableService
+      .getAllCodeMatureRating()
+      .pipe(take(1))
+      .subscribe({
+        next: (res) => {
+          this.codeMatureRatingList = res;
+        },
+        error: (err) => {
+          alert(`An error has occured. Please try again later `);
+        },
+      });
   }
 
   getAllCodeGenre() {
-    this.codesTableService.getAllCodeGenre().pipe(take(1)).subscribe({
-      next: (res) => {
-        this.codeGenreList = res;
-        this.isGenreListPopulated = true;
-      },
-      error: (err) => {
-        alert(`An error has occured. Please try again later `);
-      },
-    })
+    this.codesTableService
+      .getAllCodeGenre()
+      .pipe(take(1))
+      .subscribe({
+        next: (res) => {
+          this.codeGenreList = res;
+          this.addCodeGenreParametersIntoFormBuilder(this.codeGenreList);
+
+          console.log(this.gameSubmissionForm);
+        },
+        error: (err) => {
+          alert(`An error has occured. Please try again later `);
+        },
+      });
+  }
+
+  addCodeGenreParametersIntoFormBuilder(codeGenreList: CodesTableModel[]) {
+    codeGenreList.forEach((codeGenre) => {
+      this.gameSubmissionForm.addControl(
+        codeGenre.decodeValue,
+        new FormControl(false)
+      );
+    });
+
+    console.log(this.gameSubmissionForm);
   }
 
   getAllCodePlatform() {
-    this.codesTableService.getAllCodePlatform().pipe(take(1)).subscribe({
-      next: (res) => {
-        this.codePlatformList = res;
-      },
-      error: (err) => {
-        alert(`An error has occured. Please try again later `);
-      },
-    })
+    this.codesTableService
+      .getAllCodePlatform()
+      .pipe(take(1))
+      .subscribe({
+        next: (res) => {
+          this.codePlatformList = res;
+        },
+        error: (err) => {
+          alert(`An error has occured. Please try again later `);
+        },
+      });
   }
 
-  updateConcatenatedCodeGenre() {
-    console.log(this.gameSubmissionForm)
-    const selectedCodeGenres = this.codeGenreList.filter(cg => this.gameSubmissionForm.get(cg.code))
-      
-    console.log(selectedCodeGenres.length)
+  // updateConcatenatedCodeGenre() {
+  //   console.log(this.gameSubmissionForm);
+  //   const selectedCodeGenres = this.codeGenreList.filter((cg) =>
+  //     this.gameSubmissionForm.get(cg.code)
+  //   );
 
-  }
+  //   console.log(selectedCodeGenres.length);
+  // }
 
   submitGame() {
-    console.log(this.gameSubmissionForm.value)
+    console.log(this.gameSubmissionForm.value);
   }
   seeGameList() {
     this.router.navigate([`game/allGame`]);
--- src/app/game/model/codesTable.model.ts
+++ src/app/game/model/codesTable.model.ts
@@ -1,5 +1,11 @@
+import { FormControl } from '@angular/forms';
+
 export interface CodesTableModel {
   id: string;
   code: string;
   decodeValue: string;
+}
+
+export interface GenreFormControls {
+  [key: string]: FormControl; // Define a string index signature
 }
